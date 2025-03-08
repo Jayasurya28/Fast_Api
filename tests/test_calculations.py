1667f85cb5fa8cb7d7a0dcdb3f271c32e3aeadf4
Jayasurya28 <jayasurya1970@gmail.com>
2025-03-08 17:51:04 +0530

    test with bankaccount

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,5 +1,5 @@
 import pytest
-from app.calculations import add,subtract,multiply,divide
+from app.calculations import add,subtract,multiply,divide,BankAccount
 
 
 @pytest.mark.parametrize("num1,num2,expected",[
@@ -21,4 +21,25 @@ def test_multiply():
 def test_divide():
     assert divide(9,3) == 3
 
+def test_bank_set_initial_amount():
+    bank_account = BankAccount(50)
+    assert bank_account.balance == 50
 
+def test_bank_default_amount():
+    bank_account = BankAccount()
+    assert bank_account.balance  == 0
+
+def test_withdraw():
+    bank_account = BankAccount(50)
+    bank_account.withdraw(10)
+    assert bank_account.balance == 40
+
+def test_deposit():
+    bank_account = BankAccount(50)
+    bank_account.deposit(10)
+    assert bank_account.balance == 60
+
+def test_collect_interest():
+    bank_account = BankAccount(50)
+    bank_account.collect_interest()
+    assert round(bank_account.balance,6) == 55
