9fabf87e696c3d96adfa9ed7d90e74d621a456be
Jayasurya28 <jayasurya1970@gmail.com>
2025-03-09 00:14:05 +0530

    exception

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -30,25 +30,39 @@ def test_multiply():
 def test_divide():
     assert divide(9,3) == 3
 
-def test_bank_set_initial_amount():
-    bank_account = BankAccount(50)
+def test_bank_set_initial_amount(bank_account):
     assert bank_account.balance == 50
 
 def test_bank_default_amount(zero_bank_account):
     print("testing my bank account")
     assert zero_bank_account.balance  == 0
 
-def test_withdraw():
-    bank_account = BankAccount(50)
+def test_withdraw(bank_account):
     bank_account.withdraw(10)
     assert bank_account.balance == 40
 
-def test_deposit():
-    bank_account = BankAccount(50)
+def test_deposit(bank_account):
     bank_account.deposit(10)
     assert bank_account.balance == 60
 
-def test_collect_interest():
-    bank_account = BankAccount(50)
+def test_collect_interest(bank_account):
     bank_account.collect_interest()
     assert round(bank_account.balance,6) == 55
+
+
+@pytest.mark.parametrize("deposited,withdraw,expected",[
+    (200,100,100),
+    (50,10,40),
+    (1200,300,900)
+])
+
+def test_bank_transaction(zero_bank_account,deposited,withdraw,expected):
+    zero_bank_account.deposit(deposited)
+    zero_bank_account.withdraw(withdraw)
+    assert zero_bank_account.balance == expected
+
+
+def test_insufficient_funds(bank_account):
+    with pytest.raises(Exception):
+        bank_account.withdraw(200)
+
