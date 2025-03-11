b47fc048a096f4fa84b587e5922c45e8036af06a
Jayasurya28 <jayasurya1970@gmail.com>
2025-03-12 01:45:26 +0530

    16.12

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,11 +1,22 @@
+import pytest 
 from app import schemas
 from . database import client,session
 
-def test_root(client):
-    res = client.get("/")
-    print(res.json().get("message"))
-    assert res.json().get("message") == "Hi JS !!"
-    assert res.status_code == 200
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "aditra@gmail.com", "password": "surya"}
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 201
+    print(res.json())
+    return 
+
+
+
+# def test_root(client):
+#     res = client.get("/")
+#     print(res.json().get("message"))
+#     assert res.json().get("message") == "Hi JS !!"
+#     assert res.status_code == 200
 
 
 def test_create_user(client):
@@ -15,12 +26,7 @@ def test_create_user(client):
     assert res.status_code == 201
 
 
-def test_login_user(client):
+def test_login_user(client,test_user):
     res = client.post("/login", data={"username": "aditra@gmail.com", "password": "surya"})
     print(res.json())
-    login_res = schemas.Token(**res.json())
-    payload = oauth2.decode_token(login_res.access_token)
-    id = payload.get("user_id")
-    assert id == 1
-    assert login_res.token_type == "bearer"
     assert res.status_code == 200
