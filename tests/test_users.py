cc621c56ae4868ea06cb9fa9b6568e8e4be46810
Jayasurya28 <jayasurya1970@gmail.com>
2025-03-12 01:17:41 +0530

    test_database

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,44 +1,26 @@
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base  
-from app.main import app
 from app import schemas
-from app.config import settings
+from . database import client,session
 
-
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-client = TestClient(app)
-
-
-def test_root():
+def test_root(client):
     res = client.get("/")
     print(res.json().get("message"))
     assert res.json().get("message") == "Hi JS !!"
     assert res.status_code == 200
 
 
-def test_create_user():
-    res = client.post("/users/", json={"email": "surya@gmail.com", "password": "surya"})
+def test_create_user(client):
+    res = client.post("/users/", json={"email": "aditra@gmail.com", "password": "surya"})
     new_user = schemas.UserOut(**res.json())
-    assert new_user.email == "surya@gmail.com"
+    assert new_user.email == "aditra@gmail.com"
     assert res.status_code == 201
 
 
+def test_login_user(client):
+    res = client.post("/login", data={"username": "aditra@gmail.com", "password": "surya"})
+    print(res.json())
+    login_res = schemas.Token(**res.json())
+    payload = oauth2.decode_token(login_res.access_token)
+    id = payload.get("user_id")
+    assert id == 1
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
