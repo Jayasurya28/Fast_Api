30d9f5b9d806936d8fe0fe1ece16e3ae32447a41
Jayasurya28 <jayasurya1970@gmail.com>
2025-03-13 18:09:40 +0530

    test_posts

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,8 @@ from app.main import app
 from app.config import settings
 from app.database import get_db
 from app.database import Base
+from app.oauth2 import create_access_token
+from app import models
 from alembic import command
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost:5432/fastapi_test"
@@ -44,3 +46,46 @@ def test_user(client):
     new_user = res.json()
     new_user["password"] = user_data["password"]
     return new_user
+
+@pytest.fixture
+def token(test_user):
+    return create_access_token({"user_id": test_user["id"]})
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
+    return client
+
+
+@pytest.fixture
+def test_posts(test_user, session):
+    posts_data = [{
+        "title": "first title",
+        "content": "first content",
+        "owner_id" : test_user["id"]
+    }, {
+        "title": "second title",
+        "content": "second content",
+        "owner_id" : test_user["id"]
+    }, {
+        "title": "third title",
+        "content": "third content",
+        "owner_id" : test_user["id"]
+    }]
+
+    def create_user_model(post):
+        return models.Post(**post)
+    post_map = map(create_user_model, posts_data)
+    posts = list(post_map)
+
+    session.add_all(posts)
+
+    # session.add_all([models.Post(title= "first title",content="first content", owner_id: test_user["id"]),
+    # models.Post(title= "second title",content="second content", owner_id: test_user["id"]),
+    # models.Post(title= "third title",content="third content", owner_id: test_user["id"])])
+
+    session.commit()
+    posts = session.query(models.Post).all() 
+    return posts   
