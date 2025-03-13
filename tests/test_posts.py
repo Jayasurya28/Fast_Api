04314ef8b6f4d90f81ab6ec0c79c4cd74c1dcf15
Jayasurya28 <jayasurya1970@gmail.com>
2025-03-13 18:29:08 +0530

    testing posts

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -9,3 +9,23 @@ def test_get_all_posts(authorized_client,test_posts):
     post_list = list(post_map)
     assert len(res.json()) == len(test_posts)
     assert res.status_code == 200
+
+def test_unauthorized_user_get_all_posts(client, test_posts):
+    res = client.get("/posts/")
+    assert res.status_code == 401
+
+
+def test_unauthorized_user_get_one_posts(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
+def test_get_one_post_not_exist(authorized_client,test_posts):
+    res = authorized_client.get(f"/posts/8888")
+    assert res.status_code == 404
+
+def test_get_one_post(authorized_client,test_posts):
+    res = authorized_client.get(f"/posts/{test_posts[0].id}")
+    post = schemas.PostOut(**res.json())
+    assert post.Post.id == test_posts[0].id
+    assert post.Post.title == test_posts[0].title
